a0744a2a6d8fe4e2b9be715f28ffb5d7abce5be4
jobayer637 <web.jobayer@gmail.com>
2020-09-16 00:44:32 +0600

    LEDP day 16 implement html, css, bootstrap, owl carousul, js

---
+++ js/activate.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: 400,
+    nav: true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6
+        }
+    }
+})
